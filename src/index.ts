e4ef11bb0a0f4681b6c34e6680138846063e78c2
Abhilash Mirji <abhilashmirji1@gmail.com>
2025-03-25 11:42:05 +0530

    updated index export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export * from "./panchanga";
-export * from "./models";
+import { calculatePanchanga } from "./panchanga";
+import { PanchangaInput, PanchangaResponse } from "./models";
+
+export { calculatePanchanga };
+
+export type { PanchangaInput, PanchangaResponse };
